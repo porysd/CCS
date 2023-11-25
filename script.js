f2bd51b8219705234f2e706a30c54b3bab556c1e
porysd <84425130+porysd@users.noreply.github.com>
2023-11-25 23:05:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const tabLogos = document.querySelectorAll('.tab-logo');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabLogos.forEach((tabLogo, index) => {
+    tabLogo.addEventListener('click', () => {
+        // No need for event.preventDefault() if you want the links to navigate to the specified HTML files.
+
+        // Hide all tab contents
+        tabContents.forEach(content => content.style.display = 'none');
+        
+        // Show the selected tab content
+        tabContents[index].style.display = 'block';
+    });
+});
+
+
+var mySwiper = new Swiper('.sub-tabs', {
+    slidesPerView: 'auto',
+    loop: true,
+    initialSlide: 0, 
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+
+var tabButtons = document.querySelectorAll('.swiper-slide');
+tabButtons.forEach(function (button, index) {
+    button.addEventListener('click', function () {
+        mySwiper.slideTo(index);
+    });
+});
